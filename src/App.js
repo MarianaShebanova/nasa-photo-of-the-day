36529df674358d91d64985aad53baf2add44a30f
MarianaShebanova <mariana.shebanova@gmail.com>
2019-11-07 14:30:11 -0800

    React

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "./App.css";
 import NasaPhoto from "./NasaPhoto";
-
+import Example from "./Example";
 function App() {
   return (
     <div className="App">
       <div className="App-header"> 
           <h1> Nasa Photo </h1>
-          <NasaPhoto />
+        <Example />
       </div>
     </div>
   );
